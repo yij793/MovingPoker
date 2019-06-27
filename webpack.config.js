9abed43a96c07b3b8827ac3819d038ac368477d7
yiyao jian <jianyy1990@hotmail.com>
2019-06-26 21:49:56 -0400

    build webpack config add ts and scss

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+var distDir = path.resolve(__dirname, 'dist');
+module.exports = {
+  entry: './src/script/app.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: './dist/bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin({ distDir }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
