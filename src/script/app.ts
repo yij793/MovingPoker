6e9a8d739e0f0887b6afe6d9a688df892e2d92a6
yiyao jian <jianyy1990@hotmail.com>
2019-06-27 15:03:36 -0400

    creat 144 spirit and one civer each

--- src/script/app.ts
+++ src/script/app.ts
@@ -1,9 +1,41 @@
+//@ts-check
 import * as PIXI from 'pixi.js';
-require('../styles/style.scss');
-document.addEventListener('DOMContentLoaded', () => {
-    let renderer = PIXI.autoDetectRenderer(
-        600,
-        400,
-        {antialias: true, transparent: false, resolution: 1, backgroundColor: 0xFFFFFF}
-    );
-}, false);
+const canvas=<HTMLCanvasElement>document.getElementById('myCanvas');
+const app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    antialias: true,  
+    transparent: false,
+    resolution: 1,
+    view:canvas,
+    backgroundColor:579532
+  }
+)
+//task#1
+app.loader
+  .add('poker_10','../../assets/10D.svg')
+  .load(setup);
+function setup() {
+    const sprites=[];
+      for(let i=0;i<144;i++){
+        let sprite=new PIXI.Sprite(
+            app.loader.resources['poker_10'].texture
+          );
+          let xPositon=20+0.5*i;
+          let yPositon=50+2*i
+          sprite.position.set(xPositon,yPositon)
+            sprites.push(sprite)
+            app.stage.addChild(sprite);
+        }
+        for(let j=0;j<144;j++){
+           
+            
+        }        
+}
+
+function cardMove(){
+    for(let i=0;i<144;i++){
+
+    }
+}
+// creatSprite()
