1b61f06e7e40f6e61674cf78f08e789d1def3841
yiyao jian <jianyy1990@hotmail.com>
2019-06-28 02:08:56 -0400

    add scale for spirit in task#1

--- src/script/app.ts
+++ src/script/app.ts
@@ -13,64 +13,65 @@ const app = new PIXI.Application({
   }
 )
 app.renderer.autoResize = true
-//task#1
-// let sprites:any[]=[];
-// app.loader
-//   .add('poker_10','../../assets/10D.svg')
-//   .load(setup).on('complete',gameloop)
-// function setup() {
-//       for(let i=0;i<144;i++){
-//         let sprite=new PIXI.Sprite(
-//             app.loader.resources['poker_10'].texture
-//           );
-//           let xPositon=20+0.5*i;
-//           let yPositon=50+2*i
-//           sprite.position.set(xPositon,yPositon)
-//             sprites.push(sprite)
-//             app.stage.addChild(sprite);
-//         }
-// }
-// function gameloop(){
-//     app.ticker.autoStart = false;
-//     let j = 0;
-//     if(j===143){
-//         setInterval(function(){
-//             let sprite=sprites[j];
-//             let xv:number=(j-20)/120
-//             let yv:number=(sprite.y-sprites[143-j].y)/120
-//             app.ticker.add(function(){
-//                 if(sprite.x<20){ 
-//                     sprite.x+=xv;
-//                     sprite.y-=yv;
-//                 }
-//             })
-//             j===0? clearInterval():j--;
-//         },2000)
-//     }else{
-//     setInterval(function(){
-//     let sprite=sprites[143-j];
-//     let xv:number=(500-j)/120
-//     let yv:number=(sprite.y-sprites[j].y)/120
-//     app.ticker.add(function(){
-//         if(sprite.x<500){ 
-//             sprite.x+=xv;
-//             sprite.y-=yv;
-//         }
-//     })
-//     j>=143? clearInterval():j++;
-// },2000)
-//     }
-// }
-//task2
+// task#1
+let sprites:any[]=[];
 app.loader
-.add('club','../../assets/club.svg')
-.add('diamonds','../../assets/diamonds.svg')
-.add('jocker','../../assets/jocker.svg')
-.add('pug','../../assets/pug.svg')
-.add('spades','../../assets/spades.svg')
-.load(setup)
-function setup(){
-    let randomImage:string[]=['club','diamonds','jocker','pug','spades']
-    let randomNum:number=Math.floor(Math.random() * randomImage.length);
-    let sprite=new PIXI.Sprite(app.loader.resources[randomImage[randomNum]].texture);
+  .add('poker_10','../../assets/10D.svg')
+  .load(setup).on('complete',gameloop)
+function setup() {
+      for(let i=0;i<144;i++){
+        let sprite=new PIXI.Sprite(
+            app.loader.resources['poker_10'].texture
+          );
+          sprite.scale.set(0.3)
+          let xPositon=20+0.5*i;
+          let yPositon=50+2*i
+          sprite.position.set(xPositon,yPositon)
+            sprites.push(sprite)
+            app.stage.addChild(sprite);
+        }
 }
+function gameloop(){
+    app.ticker.autoStart = false;
+    let j = 0;
+    if(j===143){
+        setInterval(function(){
+            let sprite=sprites[j];
+            let xv:number=(j-20)/120
+            let yv:number=(sprite.y-sprites[143-j].y)/120
+            app.ticker.add(function(){
+                if(sprite.x<20){ 
+                    sprite.x+=xv;
+                    sprite.y-=yv;
+                }
+            })
+            j===0? clearInterval():j--;
+        },2000)
+    }else{
+    setInterval(function(){
+    let sprite=sprites[143-j];
+    let xv:number=(500-j)/120
+    let yv:number=(sprite.y-sprites[j].y)/120
+    app.ticker.add(function(){
+        if(sprite.x<500){ 
+            sprite.x+=xv;
+            sprite.y-=yv;
+        }
+    })
+    j>=143? clearInterval():j++;
+},2000)
+    }
+}
+//task2
+// app.loader
+// .add('club','../../assets/club.svg')
+// .add('diamonds','../../assets/diamonds.svg')
+// .add('jocker','../../assets/jocker.svg')
+// .add('pug','../../assets/pug.svg')
+// .add('spades','../../assets/spades.svg')
+// .load(setup)
+// function setup(){
+//     let randomImage:string[]=['club','diamonds','jocker','pug','spades']
+//     let randomNum:number=Math.floor(Math.random() * randomImage.length);
+//     let sprite=new PIXI.Sprite(app.loader.resources[randomImage[randomNum]].texture);
+// }
